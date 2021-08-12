2477b2ed3cec2931b8c539ceb2ddefcc3a0374e9
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-13 01:03:33 +0430

    adding secret key to cookie parser

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config();
 //Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.CPSEC));
 
 //Routes
 app.use("/api/auth/", registerRoute);
@@ -24,7 +24,7 @@ app.use("/api/auth/", loginRoute);
 //Mongodb Setup And Listen
 try {
     const connect = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true ,  useUnifiedTopology: true });
-    app.listen(process.env.PORT, () => console.log(`Server is listening on por ${process.env.PORT}`));
+    app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
 } catch (err) {
     console.log(err.message);
 }
