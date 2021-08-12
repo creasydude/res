688027749699f8c249644863eb8e9af63a100a06
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-13 01:03:49 +0430

    edit the secret key

--- utils/jwtSign.js
+++ utils/jwtSign.js
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 export const makeAccessToken = (data) => {
-    const jwtSign = jwt.sign({ _id: data._id }, process.env.atPKey, { expiresIn: '15m' });
+    const jwtSign = jwt.sign({ _id: data._id }, process.env.ATPKEY, { expiresIn: '15m' });
     return jwtSign;
 };
 
 export const makeRefreshToken = (data) => {
-    const jwtSign = jwt.sign({ _id: data._id }, process.env.rtPkey, { expiresIn: '60d' });
+    const jwtSign = jwt.sign({ _id: data._id }, process.env.RTPKEY, { expiresIn: '60d' });
     return jwtSign;
 };
