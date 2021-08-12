600011c399766923a01787e03fab3058766f9f63
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-12 18:44:35 +0430

    fix export

--- utils/jwtSign.js
+++ utils/jwtSign.js
@@ -1,13 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-const makeAccessToken = (data) => {
+export const makeAccessToken = (data) => {
     const jwtSign = jwt.sign({ _id: data._id }, process.env.atPKey, { expiresIn: '15m' });
     return jwtSign;
 };
 
-const makeRefreshToken = (data) => {
+export const makeRefreshToken = (data) => {
     const jwtSign = jwt.sign({ _id: data._id }, process.env.rtPkey, { expiresIn: '60d' });
     return jwtSign;
 };
-
-export default { makeAccessToken, makeRefreshToken };
