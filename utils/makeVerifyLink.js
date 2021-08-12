99d97089e82e0398fe932a35f884d82ff2c5e57c
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-12 19:45:00 +0430

    wrote this

---
+++ utils/makeVerifyLink.js
@@ -0,0 +1,24 @@
+import mailVerifySchema from "../dbSchemas/mailVerify.js";
+
+const makeVerifyLink = async (req, res, data) => {
+    //Declare Variables
+    const email = data.email;
+    const code = Math.floor(Math.random() * 10000000);
+    //Store In Db
+    const newEV = new mailVerifySchema({
+        email: email,
+        code: code
+    })
+    try {
+        const saveEv = await newEV.save()
+    } catch (err) {
+        throw err
+    }
+    //Make Link
+    const Link = `${req.protocol}://${req.hostname}${req.originalUrl}${email}/${code}`
+    //Implant Email Send Logic,
+    console.log(Link)
+
+}
+
+export default makeVerifyLink;
