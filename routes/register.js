e0e897e93e0e803f516acba8e9e9dba3b8561026
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-12 20:26:08 +0430

    wrote code

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,51 @@
 import express from 'express';
+import userSchema from '../dbSchemas/user.js';
+import joiValidate from '../utils/joiVerification.js';
+import { makeAccessToken, makeRefreshToken } from '../utils/jwtSign.js';
+import mailVerifySchema from "../dbSchemas/mailVerify.js";
+import makeVerifyLink from '../utils/makeVerifyLink.js';
 const Router = express.Router();
+
+Router.post('/register', async (req, res) => {
+    //Get email and pw from body
+    const { email, password } = req.body;
+    //Check if email exist
+    const mailExist = await userSchema.findOne({ email: email });
+    if (mailExist) return res.status(400).json({ message: "Mail Exist!" });
+    //Validate the email and password
+    const { error } = joiValidate(email, password)
+    if (error) return res.status(400).json({ message: error.details[0].message });
+    //Write email and password to the db
+    const newUser = new userSchema({
+        email: email,
+        password: password,
+    });
+    //Saving to the db
+    try {
+        const saveUserToDb = await newUser.save();
+        //Make Mail Verification Logic
+        const verifyMail = await makeVerifyLink(req, saveUserToDb);
+        res.status(201).json({ message: "User Successfuly Created!, Now It Needs Email Verification!" })
+    } catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
+
+})
+
+Router.post('/verifyEmail/:email/:code', async (req, res) => {
+    //Get info from params
+    const { email, code } = req.params;
+    //Check If Mail And Code Is Right
+    const checkSpecs = await mailVerifySchema.findOne({ email: email });
+    if (!checkSpecs) return res.status(400).json({ message: "Invalid Email!" });
+    if (checkSpecs?.code !== code) return res.status(400).json({ message: "Invalid Code!" });
+    //Changing status of user to active from pending.
+    try {
+        const updateStatus = await userSchema.updateOne({ email: email }, { status: "active" });
+        res.status(201).json({ message: "You Account Verified Successfuly!You Can Now Login!" });
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+})
 
 export default Router;
