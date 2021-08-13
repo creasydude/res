0f11e950a53e1eb12bdc0ad0c7dfc6e03b55cc44
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-13 13:01:17 +0430

    adding CORS to app.js

--- app.js
+++ app.js
@@ -17,13 +17,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.CPSEC));
 
+//Cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT , POST , PATCH , DELETE , GET");
+        return res.status(200)
+    }
+})
+
 //Routes
 app.use("/api/auth/", registerRoute);
 app.use("/api/auth/", loginRoute);
 
 //Mongodb Setup And Listen
 try {
-    const connect = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true ,  useUnifiedTopology: true });
+    const connect = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
 } catch (err) {
     console.log(err.message);
