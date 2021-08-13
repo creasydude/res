0a66cbb6b98a2c44e9ae15352382f4802eddfbb0
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-13 13:34:45 +0430

    added theme mode switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import { useEffect, useState } from "react";
+import Storage from 'local-storage-fallback';
+import { Route, Switch } from 'react-router-dom';
+
+const lightColors = {
+  test : "red"
+};
+
+const darkColors = {
+  test: 'gray'
+};
+
+const themes = {
+  light: lightColors,
+  dark: darkColors
+};
 
 function App() {
+  const initialTheme = () => {
+    const savedTheme = Storage.getItem('theme-mode');
+    return savedTheme ? savedTheme : 'light';
+  }
+  const [theme, setTheme] = useState(initialTheme);
+  useEffect(() => {
+    Storage.setItem('theme-mode', theme);
+  }, [theme]);
+  const themeHandler = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+
   return (
-    <div>
+    <ThemeProvider theme={themes[theme]}>
       test
-    </div>
+    </ThemeProvider>
   );
 }
 
