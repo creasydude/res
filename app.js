95f2009cc298674d1675f50be7b601c5acf24dce
Sir Creasy <mohammadicecream@yahoo.com>
2021-08-16 16:04:43 +0430

    added react to express

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cors from 'cors';
 
 //Import Routes
 import registerRoute from './routes/register.js';
 import loginRoute from './routes/login.js';
+import todoRoute from './routes/todo.js';
 
 //Dependencies
 const app = express();
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.CPSEC));
+
+//React Deploy
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 //Cors
 app.use(cors({
@@ -26,6 +37,7 @@ app.use(cors({
 //Routes
 app.use("/api/auth/", registerRoute);
 app.use("/api/auth/", loginRoute);
+app.use("/api/todo/", todoRoute)
 
 //Mongodb Setup And Listen
 try {
